8c93b4890db9d7e4ac093a7f13a7170d307f7f90
Anna Kuszal <anna.kuszal@o2.pl>
2020-04-02 13:12:08 +0200

    Add html files and fix code in server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const app = express();
 
-
 app.use((req, res, next) => {
   res.show = (name) => {
     res.sendFile(path.join(__dirname + `/views/${name}`));
@@ -11,6 +10,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/user', (req, res, next) => {
+  res.show('forbidden.html');
+});
 
 app.use(express.static(path.join(__dirname + '/public')));
 
@@ -19,25 +21,17 @@ app.get('/', (req, res) => {
   res.show('index.html');
 });
 
+app.get('/home', (req, res) => {
+  res.show('index.html');
+});
+
 app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
-app.get('/settings', (req, res) => {
-  res.show('settings.html');
-});
-
-app.get('/user/panel', (req, res) => {
-  res.show('panel.html');
-});
-
-app.get('/user/settings', (req, res) => {
-  res.show('settings.html');
-});
-
 app.use((req, res) => {
-    res.status(404).show('error.html');
-  })
+  res.status(404).show('404.html');
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
